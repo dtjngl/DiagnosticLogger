57fa710c3ca7af2ade692a8f66a9f27a04fa7bb8
dtnjgl <account@datajungle.xyz>
2024-08-25 12:23:39 +0100

    first commit

---
+++ DiagnosticLogger.config.php
@@ -0,0 +1,58 @@
+<?php namespace ProcessWire;
+
+$config = array(
+  'sender_name' => array(
+    'label' => 'Sender Name',
+    'type' => 'text',
+    'value' => '',
+    'columnWidth' => 50
+  ),
+  'sender_email' => array(
+    'label' => 'Sender Email',
+    'type' => 'email',
+    'value' => '',
+    'columnWidth' => 50,
+  ),
+  'receiver_name' => array(
+    'label' => 'Receiver Name',
+    'type' => 'text',
+    'value' => '',
+    'columnWidth' => 50
+  ),
+  'receiver_email' => array(
+    'label' => 'Receiver Email',
+    'type' => 'email',
+    'value' => '',
+    'columnWidth' => 50,
+  ),
+  'replyto_email' => array(
+    'label' => 'ReplyTo Email',
+    'type' => 'email',
+    'value' => '',
+    'columnWidth' => 50,
+  ),
+  'bcc_email' => array(
+    'label' => 'BCC Email',
+    'type' => 'email',
+    'value' => '',
+    'columnWidth' => 50,
+  ),
+  'subject_email' => array(
+    'label' => 'email subject',
+    'type' => 'text',
+    'value' => '',
+    'columnWidth' => 50,
+  ),
+  'website_name' => array(
+    'label' => 'website name',
+    'type' => 'text',
+    'value' => '',
+    'columnWidth' => 50,
+  ),
+  'email_schedule' => array(
+    'label' => 'email schedule',
+    'type' => 'text',
+    'value' => 'every2Weeks',
+    'columnWidth' => 50,
+  ),
+);  
